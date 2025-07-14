36feeee3a663b80f1148d1e26b9d9c62b96b1c7a
adam russell <adru@odoo.com>
2025-07-14 10:17:28 -0400

    [ADD] snack module

---
+++ snack/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Snack Overflow: Lunch',
+    'summary': 'Discord integration for lunch statistics',
+    'description': """
+        developed by cobr, deni, adru
+    """,
+    'author': 'cobr, deni, adru',
+    'website': "https://www.odoo.com/",
+    'version': '1.0',
+    'license': 'OPL-1',
+    'depends': ['base'],
+    'data': [
+    ],
+}
